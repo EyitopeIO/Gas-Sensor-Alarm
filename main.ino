874abfef63c5ba79bac8871316813fece6a44e41
Eyitope Adelowo <topeboy2009@gmail.com>
2019-07-28 19:39:05 +0100

    Update main.ino
    1) Featured LCD code
    2) Corrected few logic errors
    3) Corrected gas sensor code previously for mq2 to mq6

--- main.ino
+++ main.ino
@@ -2,17 +2,35 @@
 #include <EEPROM.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>   //AT commands leaving through software 
-#define TEN_SECONDS 10000
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-                                                   
-/************************************************************************************/
-//  Sandbox Electronics    2014-02-03
+#define TEN_SECONDS 10000
+#define THREE 3
+#define I2C_ADDRESS 0x27    //Run a I2C scanner code to find this
+#define BACKLIGHT_PIN 3     //Library examples declares this as 13. Beware.
+
+
+
+
+
+/*******************Demo for MQ-6 Gas Sensor Module V1.3*****************************
+Contact:  support[at]sandboxelectronics.com
+
+Lisence: Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
+
+Note:    This piece of source code is supposed to be used as a demostration ONLY. More
+         sophisticated calibration is required for industrial field application. 
+
+                                                    Sandbox Electronics    2014-02-03
+************************************************************************************/
+
 /************************Hardware Related Macros************************************/
-#define         MQ_PIN                       (0)     //define which analog input channel you are going to use
-#define         RL_VALUE                     (20)    //define the load resistance on the board, in kilo ohms
+#define         MQ_PIN                       (7)     //define which analog input channel you are going to use
+#define         RL_VALUE                     (1) //prev=20   //define the load resistance on the board, in kilo ohms
 #define         RO_CLEAN_AIR_FACTOR          (10)    //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
                                                      //which is derived from the chart in datasheet
- 
+
 /***********************Software Related Macros************************************/
 #define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
 #define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
@@ -20,11 +38,11 @@
 #define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
 #define         READ_SAMPLE_TIMES            (5)     //define the time interal(in milisecond) between each samples in 
                                                      //normal operation
- 
+
 /**********************Application Related Macros**********************************/
 #define         GAS_LPG                      (0)
 #define         GAS_CH4                      (1)
- 
+
 /*****************************Globals***********************************************/
 float           LPGCurve[3]  =  {3,   0,  -0.4};    //two points are taken from the curve. 
                                                     //with these two points, a line is formed which is "approximately equivalent"
@@ -36,102 +54,118 @@ float           CH4Curve[3]  =  {3.3, 0,  -0.38};   //two points are taken from 
                                                     //data format:{ x, y, slope}; point1: (lg2000, lg1), point2: (lg5000,  lg0.7) 
 float           Ro           =  10;                 //Ro is initialized to 10 kilo ohms
 
+/******************************************************************************************************************************* */
 
-/*******************************************************************************/
+
+
 
 
 const int MEMORY_ADDRESS_THRESHOLD = 0;   //eeprom address to keep threshold value
 const int GPSBAUD = 9600;
 
-char DATA_stream[256]; //incoming buffer from software UART
-char DATA_out[100];   //use for anything
+char DATA_in[100];   //use for anything
 String longitude;
 String lattitude;
 
-const char* telephone = "+2348034344308";
+String telephone = "\"+2348034344308\"";
 
-int GAS_SENSOR_PIN = A7;
-int BUZZER = 15;    //D12
-int LED_RED_ALARM = 8;   //D5
-int LED_GREEN_OKAY = 11;   //D8
-int BUTTON_THRESH_UP = 5;   //D2
-int BUTTON_THRESH_DOWN = 6;   //D3
-int GAS_TRUTH_VALUE = 7;    //D4
-unsigned int MQ6_gas_level = 0;   //read from analog input pin
+int BUZZER = D4;   
+int LED_RED_ALARM = D5; 
+int LED_GREEN_OKAY = D6; 
+int BUTTON_THRESH_UP = D2; 
+int BUTTON_THRESH_DOWN = D3;
 int threshold_upper;   //stored threshold in memory
 int threshold_lower;
 
-unsigned int THRESHOLD = 2100;  //ppm
+//unsigned int THRESHOLD = 2100;  //ppm
 
 int sendSMS(void);
 void beep(unsigned int duration);
 void buttonPressCheck(void);
 bool getGPSdata(void);
 void alert(void);
-void calibrateSensor();
-int sendCmdAndWait(const char* cmd, const char* resp);
+void calibrateSensor(void);
+void showReadings(void)
+//int sendCmdAndWait(String cmd, String resp);
+byte sendCmdAndWait(String cmd, String resp, SoftwareSerial object, unsigned int response_time, unsigned int trial)
 
-SoftwareSerial softUART(9,10);   //Rx, Tx
+SoftwareSerial gsm_uart(9, 10); //Tx, Rx
+SoftwareSerial gps_uart(7,8);    //Tx, Rx
 TinyGPSPlus gps;
-
-
-
+LiquidCrystal_I2C lcd(I2C_ADDRESS, 2, 1, 0, 4, 5, 6, 7, BACKLIGHT_PIN, POSITIVE);
 
 void setup()
 {
-  pinMode(GAS_SENSOR_PIN, INPUT);
+  lcd.begin(16, 2);
+  lcd.home();
+  lcd.setCursor(0,0);
+  lcd.setBacklight(HIGH);
+  lcd.print(F("Setting up...");
+
+  pinMode(MQ_PIN, INPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(LED_RED_ALARM, OUTPUT);
   pinMode(LED_GREEN_OKAY, OUTPUT);
   pinMode(BUTTON_THRESH_UP, INPUT);   //going low activates it
   pinMode(BUTTON_THRESH_DOWN, INPUT);
-  pinMode(GAS_TRUTH_VALUE, INPUT);
 
   digitalWrite(LED_GREEN_OKAY, LOW);
   digitalWrite(LED_RED_ALARM, HIGH);    //turn on red led
   beep(700);
-
-  //SETUP LCD HERE
   
   EEPROM.get(MEMORY_ADDRESS_THRESHOLD, THRESHOLD);   //retrieve saved eeprom address
 
-  softUART.begin(GPSBAUD);
-  while(!softUART);
-  getGPSdata();
-  
   Serial.begin(9600);
-  Serial.print("Calibrating...\n");
+  while(!Serial);
+  gps_uart.begin(GPSBAUD);
+  while(!gps_uart);
+  gsm_uart.begin(GPSBAUD);
+  while(!gsm_uart);
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(F("Sensor loading...");
   Ro = MQCalibration(MQ_PIN);
-  Serial.print("Calibrating is done...\n");
-  Serial.print("Ro=");
-  Serial.print(Ro);
-  Serial.print("kohm");
-  Serial.print("\n");
   
   digitalWrite(LED_RED_ALARM, LOW);
   digitalWrite(LED_GREEN_OKAY, HIGH);
 }
 
 
-
-
-
 void loop()
 {
+  showReadings();
   unsigned long time_now = millis();
   buttonPressCheck();
-  if(millis() - time_now > 30000)   //data every 30s
+  if(millis() - time_now > 10000)   //data every 30s
   {
     getGPSdata();
   }
-  if(MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_LPG) >  THRESHOLD)
+  if ( (MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_LPG) > THRESHOLD) || (MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_CH4) > THRESHOLD) )
   {
     alert();
   }
 }
 
+void showReadings(void)
+{
+  lcd.clear();
+  lcd.setCursor(0, 0); //column, row
+  lcd.print(F("LPG: "));
+  lcd.setCursor(5, 0); 
+  lcd.print(MQGetGasPercentage(MQRead(MQ_PIN) / Ro, GAS_LPG));
+  lcd.setCursor(10, 0); 
+  lcd.print(F("ppm"));
 
+  lcd.setCursor(0, 1);
+  lcd.print(F("CH4: "));
+  lcd.setCursor(5, 1); 
+  lcd.print(MQGetGasPercentage(MQRead(MQ_PIN) / Ro, GAS_CH4));
+  lcd.setCursor(10, 1);
+  lcd.print(F("ppm"));
 
+  delay(500);
+}
 
 
 void buttonPressCheck(void)
@@ -140,26 +174,36 @@ void buttonPressCheck(void)
   {
     beep(500);
     EEPROM.update(MEMORY_ADDRESS_THRESHOLD, THRESHOLD++);
-    //UPDATE LCD HERE
+    lcd.clear();
+    lcd.setCursor(0, 0);    //column, row
+    lcd.print(F("Threshold: "));
+    lcd.setCursor(0, 1);
+    lcd.print(THRESHOLD);
+    lcd.setCursor(5, 1);
+    lcd.print(F("ppm"));
+    delay(500);
   }
-  else if(digitalRead(BUTTON_THRESH_DOWN) == LOW)
+  if(digitalRead(BUTTON_THRESH_DOWN) == LOW)
   {
     beep(500);
     EEPROM.update(MEMORY_ADDRESS_THRESHOLD, THRESHOLD--);
-    //UPDATE LCD HERE
-  }
-  else
-  {
-    beep(500);
+    lcd.clear();
+    lcd.setCursor(0, 0); //column, row
+    lcd.print(F("Threshold: "));
+    lcd.setCursor(0, 1);
+    lcd.print(THRESHOLD);
+    lcd.setCursor(5, 1);
+    lcd.print(F("ppm"));
+    delay(500);
   }
 }
 
 
 bool getGPSdata(void)
 {
-  while(softUART.available() > 0)
+  while(gps_uart.available() > 0)
   {
-    if(gps.encode(softUART.read()))
+    if(gps.encode(gps_uart.read()))
     {
       if(gps.location.isValid())
       {
@@ -194,20 +238,19 @@ void beep(unsigned int duration_in_milliseconds)
 
 int sendSMS()
 {
-  if(sendCmdAndWait("AT\n","OK"))
+  if(sendCmdAndWait(F("AT\n"),F("OK"), gsm_uart, TEN_SECONDS, THREE))
   {
-    if(sendCmdAndWait("AT+CMGF=1\n","OK"))
+    if(sendCmdAndWait(F("AT+CMGF=1\n"),F("OK"), gsm_uart, TEN_SECONDS, THREE))
     {
-      emptyBuffer(DATA_out);
-      sprintf(DATA_out, "AT+CMGS=%s", telephone); 
-      if(sendCmdAndWait(DATA_out,">"))
+      String Outgoing = "AT+CMGS=" + telephone + '\n';
+      if (sendCmdAndWait(Outgoing, ">", gsm_uart, TEN_SECONDS, THREE))
       {
-       sprintf(DATA_out, "WARNING: lat=%s,lng=%s",lattitude.c_str(),longitude.c_str());
-       softUART.write(DATA_out);
-       sendCmdAndWait(char(26), "CMGS"); 
+        outgoing = "WARNING! lat,lng: " + lattitude + ',' + longitude;
+        gsm_uart.write(outgoing);
+        sendCmdAndWait(char(26), "CMGS", gsm_uart, TEN_SECONDS, THREE); 
       }
       else
-      {
+      { 
         return 0;
       }
     }
@@ -222,26 +265,52 @@ int sendSMS()
   }
 }
 
-  
-int sendCmdAndWait(const char* cmd, const char* resp)
+/*  
+int sendCmdAndWait(String cmd, String resp)
 {
-   softUART.write(cmd);
-   while(softUART.available())
+   gps.write(cmd);
+   unsigned long t_now = millis();
+   int len = resp.length();
+   int i = 0;
+   while ( (millis() - t_now) > 10 )
    {
-    int len = strlen(resp);
-    int i = 0;
-    char c = softUART.read();
-    i = (c == resp[i]) ? i++ : 0;
-    if(i == len)
+    while(gps.available())
     {
-      return 1;
-    }
-    else
-    {
-      return 0;
+      char c = gps.read();
+      i = (c == resp[i]) ? i++ : 0;
+      if(i == len)
+      {
+        return 1;
+      }
     }
    }
    return 0;
+}
+*/
+
+byte sendCmdAndWait(String cmd, String resp, SoftwareSerial object, unsigned int response_time, unsigned int trial)
+{
+  unsigned long time_now = millis();
+  int len = resp.length();
+  object.print(cmd);
+  while(trial--)
+  { 
+    while(millis() - time_now < response_time)
+    {
+      while (object.available())
+      {
+        int i, index_ = 0;
+        char c = object.read();
+        DATA_in[index_++] = c; //keeping our response just in case
+        i = (c == resp[i]) ? i++ : 0;
+        if (i == len)
+        {
+          return 1; //success
+        }
+      }
+    }
+  }
+  return 0; //fail
 }
 
 
@@ -254,19 +323,21 @@ void emptyBuffer(char* arg)
   }
 }
 
- 
+
+
+
 /****************** MQResistanceCalculation ****************************************
 Input:   raw_adc - raw value read from adc, which represents the voltage
 Output:  the calculated sensor resistance
 Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
          across the load resistor and its resistance, the resistance of the sensor
          could be derived.
-************************************************************************************/ 
+************************************************************************************/
 float MQResistanceCalculation(int raw_adc)
 {
-  return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
+  return (((float)RL_VALUE * (1023 - raw_adc) / raw_adc));
 }
- 
+
 /***************************** MQCalibration ****************************************
 Input:   mq_pin - analog channel
 Output:  Ro of the sensor
@@ -274,22 +345,23 @@ Remarks: This function assumes that the sensor is in clean air. It use
          MQResistanceCalculation to calculates the sensor resistance in clean air 
          and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about 
          10, which differs slightly between different sensors.
-************************************************************************************/ 
+************************************************************************************/
 float MQCalibration(int mq_pin)
 {
   int i;
-  float val=0;
- 
-  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
+  float val = 0;
+
+  for (i = 0; i < CALIBARAION_SAMPLE_TIMES; i++)
+  { //take multiple samples
     val += MQResistanceCalculation(analogRead(mq_pin));
     delay(CALIBRATION_SAMPLE_INTERVAL);
   }
-  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
- 
-  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro 
-                                                        //according to the chart in the datasheet 
- 
-  return val; 
+  val = val / CALIBARAION_SAMPLE_TIMES; //calculate the average value
+
+  val = val / RO_CLEAN_AIR_FACTOR; //divided by RO_CLEAN_AIR_FACTOR yields the Ro
+                                   //according to the chart in the datasheet
+
+  return val;
 }
 /*****************************  MQRead *********************************************
 Input:   mq_pin - analog channel
@@ -298,40 +370,44 @@ Remarks: This function use MQResistanceCalculation to caculate the sensor resist
          The Rs changes as the sensor is in the different consentration of the target
          gas. The sample times and the time interval between samples could be configured
          by changing the definition of the macros.
-************************************************************************************/ 
+************************************************************************************/
 float MQRead(int mq_pin)
 {
   int i;
-  float rs=0;
- 
-  for (i=0;i<READ_SAMPLE_TIMES;i++) {
+  float rs = 0;
+
+  for (i = 0; i < READ_SAMPLE_TIMES; i++)
+  {
     rs += MQResistanceCalculation(analogRead(mq_pin));
     delay(READ_SAMPLE_INTERVAL);
   }
- 
-  rs = rs/READ_SAMPLE_TIMES;
- 
-  return rs;  
+
+  rs = rs / READ_SAMPLE_TIMES;
+
+  return rs;
 }
- 
+
 /*****************************  MQGetGasPercentage **********************************
 Input:   rs_ro_ratio - Rs divided by Ro
          gas_id      - target gas type
 Output:  ppm of the target gas
 Remarks: This function passes different curves to the MQGetPercentage function which 
          calculates the ppm (parts per million) of the target gas.
-************************************************************************************/ 
+************************************************************************************/
 int MQGetGasPercentage(float rs_ro_ratio, int gas_id)
 {
-  if ( gas_id == GAS_LPG ) {
-     return MQGetPercentage(rs_ro_ratio,LPGCurve);
-  } else if ( gas_id == GAS_CH4 ) {
-      return MQGetPercentage(rs_ro_ratio,CH4Curve);
-  }    
- 
+  if (gas_id == GAS_LPG)
+  {
+    return MQGetPercentage(rs_ro_ratio, LPGCurve);
+  }
+  else if (gas_id == GAS_CH4)
+  {
+    return MQGetPercentage(rs_ro_ratio, CH4Curve);
+  }
+
   return 0;
 }
- 
+
 /*****************************  MQGetPercentage **********************************
 Input:   rs_ro_ratio - Rs divided by Ro
          pcurve      - pointer to the curve of the target gas
@@ -340,8 +416,8 @@ Remarks: By using the slope and a point of the line. The x(logarithmic value of 
          of the line could be derived if y(rs_ro_ratio) is provided. As it is a 
          logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic 
          value.
-************************************************************************************/ 
-int  MQGetPercentage(float rs_ro_ratio, float *pcurve)
+************************************************************************************/
+int MQGetPercentage(float rs_ro_ratio, float *pcurve)
 {
-  return (pow(10, (((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+  return (pow(10, (((log(rs_ro_ratio) - pcurve[1]) / pcurve[2]) + pcurve[0])));
 }
